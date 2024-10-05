4a4dd69a245d8841281914ca1988624473b944eb
Mark Burkley <mark@burkley.net>
2024-10-05 23:07:46 +0100

    updated patch files

--- test/test_one.c
+++ test/test_one.c
@@ -1,7 +1,26 @@
 #include "tap.h"
 
-int main (int argc, char *argv[]) {
-   printf ("Hello World!\n");
-   return 0;
+char a[4];
+
+void t_array (void)
+{
+    int i;
+    for (i = 0; i < 4; i++)
+        ++a[i];
+
+    test_execute (__func__, a[2]==1);
 }
 
+TESTFUNC tests[] = 
+{
+    t_array
+};
+
+#define TEST_COUNT (sizeof (tests) / sizeof (TESTFUNC))
+
+int main(void)
+{
+    test_run (tests, TEST_COUNT);
+
+    return 0;
+}
